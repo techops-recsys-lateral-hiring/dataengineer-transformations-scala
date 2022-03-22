3424fcd54dc7b6a3635f69553aafcd7aaca523ac
Srikar Ayilavarapu <asrikar@gmail.com>
2022-03-22 15:07:28 -0700

    Remove the checks for the precense of the environment variables as batect already does it for us

--- scripts/run-job.sh
+++ scripts/run-job.sh
@@ -2,29 +2,6 @@
 
 set -e
 
-err() {
-  echo "$1" >&2
-}
-
-if [[ -z ${INPUT_FILE_PATH} ]];
-then
-  err "INPUT_FILE_PATH environment variable not set"
-else
-  echo "INPUT_FILE_PATH is ${INPUT_FILE_PATH}"
-fi
-
-if [[ -z ${OUTPUT_PATH} ]];
-then
-  err "OUTPUT_PATH environment variable not set"
-else
-  echo "OUTPUT_PATH is ${OUTPUT_PATH}"
-fi
-
-if [[ -z "${OUTPUT_PATH}" || -z "${INPUT_FILE_PATH}" ]];
-then
-  exit 1
-fi
-
 JAR=target/scala-2.12/tw-pipeline_2.12-0.1.0-SNAPSHOT.jar
 
 rm -rf $OUTPUT_PATH
